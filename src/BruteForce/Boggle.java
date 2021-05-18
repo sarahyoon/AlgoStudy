0e3878eafce3c51f353e4fd91053b5db4a33e90d
sarahyoon <yoonsarah88@gmail.com>
2021-05-19 00:08:59 +0900

    Boggle: Unsolved - wrong answer

---
+++ src/BruteForce/Boggle.java
@@ -0,0 +1,105 @@
+package BruteForce;
+
+import java.util.Scanner;
+
+public class Boggle {
+
+    static int[] dx = {1, -1, 0, 0, -1, 1, -1, 1};
+    static int[] dy = {0, 0, 1, -1, -1, 1, 1, -1};
+    static String[] answer;
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int testCase = scan.nextInt();
+        scan.nextLine();
+        for(int i=0;i<testCase;i++){
+
+            String [][] board = new String[5][5];
+
+            for(int h=0;h<5;h++){
+                String lines = scan.nextLine();
+                for(int w=0;w<5;w++){
+                    board[h][w] = String.valueOf(lines.charAt(w));
+                }
+            }
+
+            int numOfWords = scan.nextInt();
+
+            String [] words = new String[numOfWords];
+            for(int n=0;n<numOfWords;n++){
+                words[n] = scan.next();
+            }
+
+            answer = new String[numOfWords];
+
+            for(int w = 0;w<numOfWords;w++){
+                answer[w] = words[w] + " " + search(words[w], board);
+            }
+
+        }
+
+        for(int i=0;i<answer.length;i++){
+            System.out.println(answer[i]);
+        }
+
+
+    }
+
+    public static String search(String word, String[][] board){
+
+        String yesorno = "";
+        Boolean find = true;
+
+
+            for(int h=0;h<board.length;h++) {
+                for (int w = 0; w < board[0].length; w++) {
+                    if (hasword(h, w, board, word)) {
+                        find = true;
+                        break;
+                    }
+                }
+                if (find) {
+                    break;
+                }
+            }
+
+        if(find){
+            yesorno = "YES";
+        }
+        else{
+            yesorno = "NO";
+        }
+
+        return yesorno;
+
+    }
+
+
+    public static Boolean hasword(int y, int x, String[][] board, String cha){
+
+
+        if(x<0 || y<0 || x >= 5 || y >= 5){
+            return false;
+        }
+
+        if(board[y][x] != String.valueOf(cha.charAt(0))){
+            return false;
+        }
+
+        if(cha.length() == 1){
+            return true;
+        }
+
+        for(int i=0;i<8;i++){
+            int next_y = y + dy[i];
+            int next_x = x + dx[i];
+
+            if(hasword(next_y, next_x, board, cha.substring(1))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
