73cd3588a766986d09907de4cc6a3942041b9e7c
sarahyoon <yoonsarah88@gmail.com>
2021-05-25 23:12:17 +0900

    LIS - solved

---
+++ src/BruteForce/LIS.java
@@ -0,0 +1,48 @@
+package BruteForce;
+
+import java.util.Scanner;
+
+public class LIS {
+
+    static String ans;
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        ans = "";
+        int testCase = scan.nextInt();
+
+        for(int i=0;i<testCase;i++){
+
+            int n = scan.nextInt();
+
+            int [] a = new int[n];
+
+            for(int k=0;k<n;k++){
+                a[k] = scan.nextInt();
+            }
+
+            int[] dp= new int[n];
+
+            for(int k=0;k<n;k++){
+                dp[k] = 1;
+                for(int j=0;j<k;j++){
+                    if(a[j] < a[k] && dp[k] < dp[j]+1){
+                        dp[k] = dp[j]+1;
+                    }
+                }
+            }
+
+            int result = dp[0];
+            for(int k=0;k<n;k++){
+                if(result<dp[k]){
+                    result = dp[k];
+                }
+            }
+
+            ans += result + "\n";
+
+        }
+
+        System.out.println(ans);
+    }
+}
