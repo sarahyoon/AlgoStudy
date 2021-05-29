4b7abb8f8d89b49f4a77f81c6dd1d7a826c56eb2
sarahyoon <yoonsarah88@gmail.com>
2021-05-29 23:14:51 +0900

    PI - solved Change code for following solution from the book.

--- src/BruteForce/PI.java
+++ src/BruteForce/PI.java
@@ -6,113 +6,102 @@ import java.util.Scanner;
 public class PI {
 
     static int INF = 987654321;
-    static int[] numbers;
-    static int[] result;
-    static int[][] levels;
+    static String N ;
+    static int[] dp;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        int testCase = sc.nextInt();
+        int testCase = scan.nextInt();
+        scan.nextLine();
 
         for(int t=0; t<testCase; t++){
 
-            String str = sc.next();
-            numbers = new int[str.length()];
-            result = new int[str.length()];
-            levels = new int[str.length()][3];
+            N = scan.nextLine();
+            dp = new int[N.length()];
+            Arrays.fill(dp, -1);
 
-            for(int i=0; i<str.length(); i++) {
-                numbers[i] = Integer.parseInt(String.valueOf(str.charAt(i)));
+            System.out.println(memorize(0));
+        }
+    }
+
+
+    public static int memorize(int begin) {
+
+        if(begin == N.length()){
+            return 0;
+        }
+
+
+        if(dp[begin] != -1){
+            return dp[begin];
+        }
+
+        int ret = INF;
+
+        for(int i = 3;i<=5;i++){
+            if(begin+i <= N.length()){
+                ret = dp[begin] = Math.min(ret, memorize(begin+i) + classify(begin, begin+i-1));
             }
-            System.out.println(totalMinLevel(0));
-        }
-    }
-
-
-    public static int totalMinLevel(int start){
-        if(result[start] != 0)
-            return result[start];
-
-        result[start] = INF;
-
-        for(int length = 3; length <= 5; length++){
-            if(start + length < numbers.length)
-                result[start] = Math.min(result[start], totalMinLevel(start + length) + currentLevel(start, length));
-            else if(start + length == numbers.length)
-                result[start] = Math.min(result[start], currentLevel(start, length));
-        }
-        return result[start];
-    }
-
-    public static int currentLevel(int start, int length){
-        if(levels[start][length - 3] != 0)
-            return levels[start][length - 3];
-
-        levels[start][length - 3] = 10;
-
-        if(isLevelOne(start, start + length))
-            levels[start][length - 3] = 1;
-        else if(isLevelTwo(start, start + length)){
-            levels[start][length - 3] = 2;
-        }else if(isLevelFour(start, start + length)) {
-            levels[start][length - 3] = 4;
-        }else if(isLevelFive(start, start + length)){
-            levels[start][length - 3] = 5;
-        }
-        return levels[start][length - 3];
-    }
-
-    public static boolean isLevelOne(int start, int end){
-        int first = numbers[start];
-
-        for(int i=start + 1; i<end; i++){
-            if(numbers[i] != first)
-                return false;
-        }
-        return true;
-    }
-
-    public static boolean isLevelTwo(int start, int end){
-        int diff = numbers[start + 1] - numbers[start];
-
-        if(diff == 1 || diff == -1)
-            return isSequence(start, end, diff);
-        return false;
-    }
-
-    public static boolean isLevelFour(int start, int end) {
-        int[] repeatNumbers = {numbers[start], numbers[start + 1]};
-
-        int count = 0;
-
-        for(int i=start + 2; i<end; i++){
-            if(numbers[i] != repeatNumbers[count])
-                return false;
-
-            count^=1;
-        }
-        return true;
-    }
-
-    public static boolean isLevelFive(int start, int end){
-        int diff = numbers[start + 1] - numbers[start];
-        return isSequence(start, end, diff);
-    }
-
-    public static boolean isSequence(int start, int end, int diff) {
-        int prev = numbers[start + 1];
-
-        for(int i=start + 2; i<end; i++){
-            if(!(numbers[i] - prev == diff))
-                return false;
-
-            prev = numbers[i];
         }
 
-        return true;
+        return ret;
     }
 
+    public static int classify(int a, int b){
+
+        String M = N.substring(a, b+1);
+
+        // level 1: all elements are same.
+        Boolean isSame = true;
+        for(int i=0;i<M.length();i++){
+            if(M.charAt(0) != M.charAt(i)) {
+                isSame = false;
+                break;
+            }
+        }
+
+        if(isSame){
+            return 1;
+        }
+
+
+        // level 2: Either numbers are increasing or decreasing by 1.
+        Boolean progressive = true;
+        int diff = M.charAt(1) - M.charAt(0);
+        for(int i=0;i<M.length()-1;i++){
+            if(diff != M.charAt(i+1) - M.charAt(i)){
+                progressive = false;
+                break;
+            }
+        }
+
+        if(progressive && (diff == 1 || diff == -1 )){
+            return 2;
+        }
+
+        // level 5: Numbers are increasing / decreasing in same diff.
+        if(progressive){
+            return 5;
+        }
+
+        // level 4: 2 numbers are alternative
+        Boolean alternative = true;
+        for(int i=0;i<M.length();i++){
+            if(M.charAt(i) != M.charAt(i%2)){
+                alternative = false;
+                break;
+            }
+        }
+
+        if(alternative){
+            return 4;
+        }
+
+        //or else
+        return 10;
+
+    }
 
 
 }
