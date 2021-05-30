bae7889b85c6b3255ec96831b11e9eb09f002d5d
sarahyoon <yoonsarah88@gmail.com>
2021-05-30 16:49:30 +0900

    LIS_Code Modified

--- src/BruteForce/LIS/LIS.java
+++ src/BruteForce/LIS/LIS.java
@@ -1,48 +1,49 @@
 package BruteForce.LIS;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LIS {
 
-    static String ans;
+    static int[] a;
+    static int[] cache;
+    static int n;
     public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
-        ans = "";
         int testCase = scan.nextInt();
 
         for(int i=0;i<testCase;i++){
 
-            int n = scan.nextInt();
+            n = scan.nextInt();
 
-            int [] a = new int[n];
+            a = new int[n];
+            cache = new int[n+1];
+            Arrays.fill(cache, -1);
 
             for(int k=0;k<n;k++){
                 a[k] = scan.nextInt();
             }
 
-            int[] dp= new int[n];
-
-            for(int k=0;k<n;k++){
-                dp[k] = 1;
-                for(int j=0;j<k;j++){
-                    if(a[j] < a[k] && dp[k] < dp[j]+1){
-                        dp[k] = dp[j]+1;
-                    }
-                }
-            }
-
-            int result = dp[0];
-            for(int k=0;k<n;k++){
-                if(result<dp[k]){
-                    result = dp[k];
-                }
-            }
-
-            ans += result + "\n";
-
+            System.out.println(lis(-1)-1);
         }
 
-        System.out.println(ans);
+    }
+
+    public static int lis(int start){
+
+            if(cache[start+1] != -1){
+                return cache[start+1];
+            }
+
+            cache[start+1] = 1;
+
+            for(int next = start+1;next<n;++next){
+                if(start == -1 || (a[start]<a[next])){
+                    cache[start+1] = Math.max(cache[start+1], lis(next)+1);
+                }
+            }
+        return cache[start+1];
+
     }
 }
